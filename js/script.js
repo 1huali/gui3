9ac2a99b5145a21871665a7d5a281eee03f304b3
1huali <wanhuali2000@gmail.com>
2022-04-16 17:24:31 -0400

    drafties

--- js/script.js
+++ js/script.js
@@ -15,21 +15,65 @@ window.onload = function () {
 
     //creation of images
 let imgArray = [];
-let img1 = document.getElementById(`img1`);
-let img2 = document.getElementById(`img2`);
-let img3 = document.getElementById(`img3`);
+// let img1 = document.getElementById(`img1`);
+// let img2 = document.getElementById(`img2`);
+// let img3 = document.getElementById(`img3`);
+// imgArray.push(`img1`);
+// imgArray.push(`img2`);
+// imgArray.push(`img3`);
 
-imgArray.push(`img1`);
-imgArray.push(`img2`);
-imgArray.push(`img3`);
-
+let imgDiv= document.getElementById(`imgCollection`);
 
 //creation of titles/text elements
 let state = `intro`
 let introText = document.getElementById(`titleText`);
 
 
+function stateChange(){
+state= `intro`;
 
+if (state === `intro`){
+
+    // only title appearing, and disappears after 3 seconds
+        imgDiv.style.visibility = "hidden"; 
+
+        setTimeout(function () {
+            displayTitle;
+          }, 2000);
+          displayLookbook();
+}
+
+if (state=== `lookbook`){
+    setTimeout(displayLookbook(),2500);
+
+}
+
+if (state=== `onTimer`){
+    setTimeout(window.close(),70000);
+}
+setTimeout(function({
+    state= `endTitle`;
+}),65000);
+}
+}
+
+function displayTile(){
+    introText.style.visibility = "visible"
+}
+
+function displayLookbook(){
+    for (let i=0; i< imgArray.length; i++){
+        imgDiv.style.visibility = "visible"; 
+        imgArray[i].style.opacity="100%";
+//Q: how to make sure they appear one after the other?
+if (imgArray[i].style.opacity="100%"){
+state = `onTimer` }
+}
+}
+
+function endTitle(){
+    endTitle.style.visibility="visible"
+}
 
 
 } //end onLoad
