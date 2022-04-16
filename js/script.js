edd6b12bd0f229a03207683f0b1a1c39dc3873e0
1huali <wanhuali2000@gmail.com>
2022-04-16 16:07:38 -0400

    Creation of html el in js ; img as placeholder

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+gui3 capsule collection
+state intro : demons belong on earth, and so do U
+- show all images in a setTimeout(displayImage(),2000);
+- space for the lil description
+- demons all over city
+- close tab
 */
 
 "use strict";
 
-// Code goes here
+window.onload = function () {
+
+
+
+    //creation of images
+let imgArray = [];
+let img1 = document.getElementById(`img1`);
+let img2 = document.getElementById(`img2`);
+let img3 = document.getElementById(`img3`);
+
+imgArray.push(`img1`);
+imgArray.push(`img2`);
+imgArray.push(`img3`);
+
+
+//creation of titles/text elements
+let state = `intro`
+let introText = document.getElementById(`titleText`);
+
+
+
+
+
+} //end onLoad
